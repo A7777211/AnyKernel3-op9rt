590ccb7001f4254a9c3a9bea874d3d8277dd14eb
A7777 <103983619+A7777211@users.noreply.github.com>
2023-04-04 22:46:03 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Install KernelSU for Oneplus 9RT
+kernel.string=KernelSU for Oneplus 9RT
 do.devicecheck=1
 do.modules=0
 do.systemless=0
